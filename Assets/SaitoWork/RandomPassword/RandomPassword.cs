d389dcea4d8d56545781177d2035b15fd8d62435
ss-tt <st3110z214@gmail.com>
2020-09-17 17:18:10 +0900

    タッチパネルでパスワードを入力するとボールとメモが出現する ,ランダムパスワード作成

---
+++ Assets/SaitoWork/RandomPassword/RandomPassword.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPassword : MonoBehaviour
+{
+    /// <summary>
+    /// Written Saito Takahiro
+    /// </summary>
+    public static RandomPassword instance;
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public string password_A, password_B, password_C;
+    public string KeyPassword;
+    void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if ((i % 3) == 0 && i != 0)//012,345,678
+            {
+                KeyPassword += ",";
+            }
+            KeyPassword += Random.Range(0, 10);
+        }
+        string[] SplitKeyPassword = KeyPassword.Split(',');
+        password_A = SplitKeyPassword[0];
+        password_B = SplitKeyPassword[1];
+        password_C = SplitKeyPassword[2];
+        KeyPassword = KeyPassword.Replace(",", "");
+    }
+}
