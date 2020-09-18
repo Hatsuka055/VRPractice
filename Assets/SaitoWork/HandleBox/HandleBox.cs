f375ad1cf35fd24d67537cac8a30f7a8775fff84
ss-tt <st3110z214@gmail.com>
2020-09-18 17:58:07 +0900

    蓋を開けるとパスワードが表示されるように変更　パスワード取得表示用スクリプト作成

--- Assets/SaitoWork/HandleBox/HandleBox.cs
+++ Assets/SaitoWork/HandleBox/HandleBox.cs
@@ -7,16 +7,26 @@ public class HandleBox : MonoBehaviour
     [SerializeField] private GameObject HandlePivot = default;
     [SerializeField] private GameObject Hand = default;
     [SerializeField] private GameObject futaPivot = default;
+    [SerializeField] private GameObject PasswordObject = default;
+
 
     private void Update()
     {
         if (90 <= handleAngle)
         {
             futaPivot.transform.rotation = Quaternion.Slerp(futaPivot.transform.rotation, Quaternion.Euler(90, 0, 0), 2 * Time.deltaTime);
+            if (!PasswordObject.activeSelf)
+            {
+                PasswordObject.SetActive(true);
+            }
         }
         else
         {
             futaPivot.transform.rotation = Quaternion.Slerp(futaPivot.transform.rotation, Quaternion.Euler(0, 0, 0), 2 * Time.deltaTime);
+            if (PasswordObject.activeSelf)
+            {
+                PasswordObject.SetActive(false);
+            }
         }
 
     }
@@ -26,8 +36,8 @@ public class HandleBox : MonoBehaviour
     {
         if (other.gameObject.name == "THE HAND")
         {
-            Zangle = Hand.transform.localRotation.eulerAngles.z;
-            if (Zangle < 91)
+            Zangle = Hand.transform.localRotation.z;
+            if (0 < Zangle)
             {
                 HandlePivot.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Clamp(Hand.transform.localRotation.eulerAngles.z, 0, 90));
                 handleAngle = HandlePivot.transform.localRotation.eulerAngles.z;
