9346ec1b70f3b1d6253e564953e54fe4aac89948
Hatsuka055 <yhfmx698@gmail.com>
2020-09-18 17:16:34 +0900

    Grabber、Grabbableの継承スクリプトの追加 okazakiフォルダ内 作業退避用コミット

---
+++ Assets/okazaki/EscapegameGrabber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapegameGrabber : OVRGrabber
+{
+	// Start is called before the first frame update
+/*	private void Start()
+	{
+		base.Start();
+	}
+	override protected  void GrabBegin()
+    {
+        base.GrabBegin();
+    }*/
+
+	override protected void OffhandGrabbed(OVRGrabbable grabbable){
+		//両手持ち可能なGrabbableだった場合、
+		//1,もう片方の手の情報を渡す
+		//2,拡大縮小モードをONにする
+		if (m_grabbedObj  == grabbable){
+			if (grabbable.GetComponent<OffhandGrabbable>()) {
+				var cast = grabbable.GetComponent<OffhandGrabbable>();
+				cast.ReserveOffhand(this);
+				cast.ScaleEnable(true);
+				GrabbableRelease(Vector3.zero, Vector3.zero);
+			}
+			else
+			{
+				GrabbableRelease(Vector3.zero, Vector3.zero);
+			}
+		}
+    }
+}
---
+++ Assets/okazaki/OffhandGrabbable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffhandGrabbable : OVRGrabbable
+{
+	protected OVRGrabber offhand;
+	bool isScalable = false;
+	float initDistance = 1;
+	OVRGrabber reservedOffhand
+	{
+		get { return offhand; }
+	}
+
+	public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+	{
+		if (this.grabbedBy && this.reservedOffhand)
+		{
+
+		}
+
+		base.GrabEnd(linearVelocity, angularVelocity);
+
+	}
+
+	public void ReserveOffhand(EscapegameGrabber offhand)
+	{
+		this.offhand = offhand;
+		this.initDistance = Vector3.Distance(offhand.transform.position, this.grabbedBy.transform.position);
+		
+	}
+	public void ScaleEnable(bool scalable)
+	{
+		this.isScalable = scalable;
+	}
+	void Update()
+	{
+		if (isScalable)
+		{
+			//ここに拡大縮小のロジックを書く
+			float grabbingDistance = Vector3.Distance(offhand.transform.position,
+															this.grabbedBy.transform.position);
+
+			this.transform.position = (	offhand.transform.position 
+										+ this.grabbedBy.transform.position
+										) / 2;
+			this.transform.localScale *= grabbingDistance / initDistance;
+		}
+
+	}
+}
