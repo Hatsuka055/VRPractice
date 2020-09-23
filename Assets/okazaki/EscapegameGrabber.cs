c0a50c3d8453cf1f540fe683133e0eb648b9b5ca
Hatsuka055 <yhfmx698@gmail.com>
2020-09-23 11:11:47 +0900

    両手持ち・拡大縮小機能実装（バグ修正未完）

--- Assets/okazaki/EscapegameGrabber.cs
+++ Assets/okazaki/EscapegameGrabber.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class EscapegameGrabber : OVRGrabber
 {
 	// Start is called before the first frame update
-/*	private void Start()
-	{
-		base.Start();
-	}
-	override protected  void GrabBegin()
-    {
-        base.GrabBegin();
-    }*/
+	/*	private void Start()
+		{
+			base.Start();
+		}
+		override protected  void GrabBegin()
+		{
+			base.GrabBegin();
+		}*/
+	[SerializeField] EscapegameGrabber conRight;
+	[SerializeField] EscapegameGrabber conLeft;
 
 	override protected void OffhandGrabbed(OVRGrabbable grabbable){
 		//両手持ち可能なGrabbableだった場合、
@@ -21,8 +23,9 @@ public class EscapegameGrabber : OVRGrabber
 		if (m_grabbedObj  == grabbable){
 			if (grabbable.GetComponent<OffhandGrabbable>()) {
 				var cast = grabbable.GetComponent<OffhandGrabbable>();
-				cast.ReserveOffhand(this);
+				cast.InitializeDistance(conRight,conLeft);
 				cast.ScaleEnable(true);
+				cast.reservedHand = (EscapegameGrabber) grabbable.grabbedBy;
 				GrabbableRelease(Vector3.zero, Vector3.zero);
 			}
 			else
@@ -31,4 +34,26 @@ public class EscapegameGrabber : OVRGrabber
 			}
 		}
     }
+	public void executeGrabBegin()
+	{
+		base.GrabBegin();
+	}
+	public void executeGrabEnd()
+	{
+		base.GrabEnd();
+	}
+	public void executeGrabableRelease()
+	{
+		base.GrabbableRelease(Vector3.zero,Vector3.zero);
+	}
+	public OVRInput.Controller offhand
+	{
+		get { return this.m_controller; }
+	}
+	//EscapegameGrabberでgripTransformを取得するためにprotectedを強引に回避
+	public Transform attachedGriptransform
+	{
+		get { return m_gripTransform; }
+	}
+
 }
--- Assets/okazaki/OffhandGrabbable.cs
+++ Assets/okazaki/OffhandGrabbable.cs
@@ -1,51 +1,131 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+using UnityEditor;
 
 public class OffhandGrabbable : OVRGrabbable
 {
-	protected OVRGrabber offhand;
+	
+	protected EscapegameGrabber offhand;
 	bool isScalable = false;
-	float initDistance = 1;
-	OVRGrabber reservedOffhand
+	[SerializeField]float initDistance = 1.11111f;
+	[SerializeField] EscapegameGrabber reserved;
+
+	Vector3 scaleBuffer;
+
+	//grabbedByを継承クラスにキャストするためのプロパティ
+	EscapegameGrabber grabbedBy_4Escapegame
 	{
-		get { return offhand; }
+		set { m_grabbedBy = value; }
+		get { return (EscapegameGrabber)this.grabbedBy; }
+	}
+
+	//最初に掴んでいた方の手をオブジェクトが記憶するためのプロパティ
+	public EscapegameGrabber reservedHand
+	{
+		set
+		{
+			if (value is EscapegameGrabber)
+			{
+				this.reserved = value;
+			}
+		}
+
+		get { return this.reserved; }
 	}
 
 	public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
 	{
-		if (this.grabbedBy && this.reservedOffhand)
+		
+		if (isScalable)
 		{
+			
 
 		}
 
 		base.GrabEnd(linearVelocity, angularVelocity);
 
 	}
-
-	public void ReserveOffhand(EscapegameGrabber offhand)
+	
+	public void InitializeDistance(EscapegameGrabber right,EscapegameGrabber left)
 	{
-		this.offhand = offhand;
-		this.initDistance = Vector3.Distance(offhand.transform.position, this.grabbedBy.transform.position);
-		
+		scaleBuffer = this.transform.localScale;
+		if(this.grabbedBy == right)
+		{
+			this.initDistance = Vector3.Distance(left.attachedGriptransform.position, 
+												this.grabbedBy_4Escapegame.attachedGriptransform.position
+												);
+			this.offhand = right;
+		}else if(this.grabbedBy == left)
+		{
+			this.initDistance = Vector3.Distance(right.transform.position,
+												this.grabbedBy_4Escapegame.attachedGriptransform.position
+												);
+			this.offhand = left;
+
+		}
+
 	}
+
 	public void ScaleEnable(bool scalable)
 	{
 		this.isScalable = scalable;
 	}
 	void Update()
 	{
+		
+		//掴んでる物があり、かつもう片方の手もつかんでる時
+//		if (grabbedBy_4Escapegame && this.offhand) {
+			float triggerBuffer = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, this.offhand.offhand);
+			float triggerBuffer2 = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, this.grabbedBy_4Escapegame.offhand);
+			if (triggerBuffer <= this.offhand.grabEnd || triggerBuffer2 <= this.offhand.grabEnd)
+			{
+				Debug.Log("後からつかんだ方の手を放した");
+				if(!grabbedBy_4Escapegame )
+				{
+					grabbedBy_4Escapegame = offhand;
+					GrabEnd(Vector3.zero,Vector3.zero);
+				}
+				this.offhand = null;
+				ScaleEnable(false);
+					
+			}
+			
+//		}
+
 		if (isScalable)
 		{
-			//ここに拡大縮小のロジックを書く
-			float grabbingDistance = Vector3.Distance(offhand.transform.position,
-															this.grabbedBy.transform.position);
+			
+			if(grabbedBy_4Escapegame != this.offhand)
+			{
 
-			this.transform.position = (	offhand.transform.position 
-										+ this.grabbedBy.transform.position
-										) / 2;
-			this.transform.localScale *= grabbingDistance / initDistance;
+				//ここに拡大縮小のロジックを書く
+				float grabbingDistance = Vector3.Distance(offhand.transform.position,
+																this.grabbedBy.transform.position);
+				Debug.Log("Update時offhand = " + this.offhand.name);
+				Debug.Log("Update時grabbedBy = " + grabbedBy.name);
+				Debug.Log(grabbedBy.name + "と" + offhand.name + "の距離 " + grabbingDistance);
+				Debug.Log("両手でつかんでる:"+ grabbingDistance);
+				this.transform.position = (	offhand.attachedGriptransform.position 
+											+ this.grabbedBy_4Escapegame.attachedGriptransform.position
+											) / 2;
+				Debug.Log("initDistance = " + this.initDistance);
+				float scale = grabbingDistance / this.initDistance;
+				Debug.Log("localscale=" + this.transform.localScale);
+				this.transform.localScale = new Vector3(
+					this.scaleBuffer.x * scale,
+					this.scaleBuffer.y * scale,
+					this.scaleBuffer.z * scale
+				);
+				Debug.Log("スケール："+this.transform.localScale);
+			}
+
+			
+			
 		}
+
 
 	}
 }
