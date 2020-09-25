2d25f85f7b326821aec184bd6248e9f31aa098e0
ss-tt <st3110z214@gmail.com>
2020-09-25 18:27:35 +0900

    マージ前コミット

---
+++ Assets/Yamashita/BallThrow_脱出ゲーム処理/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+    [SerializeField]
+    private GameObject rod;
+
+    private void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Ball") {
+            //棒に重力を反映させる
+            rod.GetComponent<Rigidbody>().useGravity = true;
+            rod.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }//if
+    }//OnCollisionEnter
+
+}//Target
---
+++ Assets/Yamashita/LogDisplay_脱出ゲーム処理/LogButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogButton : MonoBehaviour {
+    public Text log_LogButton;
+    private Text logText;
+    private void Start() {
+        logText = log_LogButton.GetComponent<Text>();
+    }
+    public void OnClick() {
+        if (this.logText.enabled)
+            this.logText.enabled = false;
+        else
+            this.logText.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (this.logText.enabled)
+            this.logText.enabled = false;
+        else
+            this.logText.enabled = true;
+    }
+}
