9749683e87363ad64a29f706829eb8959264da79
yamashita_aki <yamashita-a@m.sus-g.co.jp>
2020-09-30 19:25:40 +0900

    シーン遷移の作成

---
+++ Assets/Yamashita/Title/SceneChange.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+ public void OnClick() {
+  SceneManager.LoadScene(1);//File→BuildSettings→ScenesInBuildでメインステージを設定してください
+ }//OnClick
+}//SceneChange
