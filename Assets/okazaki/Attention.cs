49ecca8554a74df97d68071e31dab510b8389cd5
Hatsuka055 <yhfmx698@gmail.com>
2020-10-18 18:19:11 +0900

    注意喚起イメージ点滅実装 扉の鍵穴イメージを追加 ボールの大きさを修正 パスワード表示の順番を明示 ナンバーパネルの動作修正：触れたボタンが消える、間違えたら前ボタン復活でやり直しなど 以上に関連したPrefabの編集

--- Assets/okazaki/Attention.cs
+++ Assets/okazaki/Attention.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 public class Attention : MonoBehaviour
 {
 
 	[SerializeField]
 	Transform headPositon;
 	[SerializeField]
+	Transform parentTrans;
+	[SerializeField]
 	float distance;
-	Color color;
+	[SerializeField]
+	float alpha;
+	[SerializeField]
+	Color m_color;
 	[SerializeField]
 	Image image;
-	public float hoge;
 	
 	// Start is called before the first frame update
     void Start()
     {
 		if (!headPositon)
 		{
-			GameObject.Find("CenterEyeAnchor");
+			headPositon =  GameObject.Find("CenterEyeAnchor").transform;
 		}
-		color = GetComponent<Color>();
-		image = GetComponent<Image>();
+		alpha = 0f;
+		image = this.GetComponent<Image>();
+		parentTrans = this.transform.parent.parent;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-	//	if (Vector3.Distance(this.transform.position, headPositon.position) < distance)
+		if (Vector3.Distance(this.parentTrans.position, headPositon.position) < distance)
 		{
-			color.a = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup))*100;
-			image.tintColor = color;
+			alpha = Mathf.Abs(Mathf.Sin(Time.realtimeSinceStartup));
+			image.color = new Color(image.color.r, image.color.g, image.color.g, alpha);
+
+		}else
+		{
+			alpha = 0f;
+			image.color = new Color(image.color.r, image.color.g, image.color.g, alpha);
 		}
+
     }
 }
